9dc1780b8fb3cd805a7f40246c9baf97daed9f62
g0dm0d <45468452+g0dm0d@users.noreply.github.com>
2021-10-25 15:24:59 +0300

    Update Bot10.py

--- Bot10.py
+++ Bot10.py
@@ -9,4 +9,4 @@ client = commands.Bot(command_prefix = '!')
 async def on_message(ctx):
 	if ctx.author.id == 279311290688602112:
 		await ctx.channel.send('<@!277490621264953345> https://youtu.be/w1bz5PlvOMc')
-client.run('ODQzMjE1Mzk1ODk1NzcxMTY2.YKAniA.gY7C0dGm_RqGDOaEdeurYdHH9fE')
+client.run('token')
